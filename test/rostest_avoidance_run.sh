45bc2b8add70f63d819f74baecee48c51090c632
Anthony Lamping <lamping.ap@gmail.com>
2018-11-01 13:28:41 -0400

    test: avoidance: add GAZEBO_RESOURCE_PATH

--- test/rostest_avoidance_run.sh
+++ test/rostest_avoidance_run.sh
@@ -16,6 +16,7 @@ source /usr/share/gazebo/setup.sh
 
 export CATKIN_SETUP_UTIL_ARGS=--extend
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:${PX4_SRC_DIR}/catkin_ws/src/avoidance/sim/models
+export GAZEBO_RESOURCE_PATH=$GAZEBO_RESOURCE_PATH:${PX4_SRC_DIR}/catkin_ws/src/avoidance/sim/worlds
 
 source $DIR/rostest_px4_run.sh "$@"
 
